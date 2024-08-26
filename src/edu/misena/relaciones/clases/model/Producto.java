3f3054cd5ba5f810e55eb0e3e8afff850dc091bf
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-26 07:03:55 -0500

    Creacion de los getters y setters del producto

--- src/edu/misena/relaciones/clases/model/Producto.java
+++ src/edu/misena/relaciones/clases/model/Producto.java
@@ -4,4 +4,14 @@ public class Producto {
     private String codigo;
     private String nombre;
     private double precio;
+
+    //getters//
+    public String getCodigo(){return codigo;}
+    public String getNombre(){return nombre;}
+    public double getPrecio(){return precio;}
+
+    //seters//
+    public void setCodigo(String codigo) {this.codigo = codigo;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setPrecio(double precio) {this.precio = precio;}
 }
