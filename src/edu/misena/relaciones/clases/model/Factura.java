13b2f7ceec4ac2a011c52f51429249ced48d866a
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-26 07:13:51 -0500

    Creacion de los getters y setters de la factura

--- src/edu/misena/relaciones/clases/model/Factura.java
+++ src/edu/misena/relaciones/clases/model/Factura.java
@@ -8,4 +8,18 @@ public class Factura {
     private Date fecha;
     private Cliente cliente;
     private itemFactura[] items;
+
+    //getters//
+    public int getFolio() {return folio;}
+    public String getDescripcion() {return descripcion;}
+    public Date getFecha() {return fecha;}
+    public Cliente getCliente() {return cliente;}
+    public itemFactura[] getItems() {return items;}
+
+    //setters//
+    public void setFolio(int folio) {this.folio = folio;}
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+    public void setFecha(Date fecha) {this.fecha = fecha;}
+    public void setCliente(Cliente cliente) {this.cliente = cliente;}
+    public void setItems(itemFactura[] items) {this.items = items;}
 }
