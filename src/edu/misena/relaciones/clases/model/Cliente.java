922cd8699eb5c49665f5656bc8b2a0b35509ca73
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-26 07:00:51 -0500

    Creacion de los getters y setters del cliente

--- src/edu/misena/relaciones/clases/model/Cliente.java
+++ src/edu/misena/relaciones/clases/model/Cliente.java
@@ -3,4 +3,13 @@ package edu.misena.relaciones.clases.model;
 public class Cliente {
     private String nombre;
     private String nif; // Número de Identificación Fiscal
+
+    //geters//
+    public String getNombre(){return nombre;}
+    public String getNif(){return nif;}
+
+    //seters//
+
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setNif(String nif) {this.nif = nif;}
 }
