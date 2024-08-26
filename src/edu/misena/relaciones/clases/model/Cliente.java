f5483bff3a021f4f28337ed599b3d0ea5906523e
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-26 07:14:47 -0500

    Creacion del constructor de cliente

--- src/edu/misena/relaciones/clases/model/Cliente.java
+++ src/edu/misena/relaciones/clases/model/Cliente.java
@@ -4,6 +4,11 @@ public class Cliente {
     private String nombre;
     private String nif; // Número de Identificación Fiscal
 
+    public Cliente(String nombre,String nif){
+        this.nombre=nombre;
+        this.nif=nif;
+    }
+
     //geters//
     public String getNombre(){return nombre;}
     public String getNif(){return nif;}
