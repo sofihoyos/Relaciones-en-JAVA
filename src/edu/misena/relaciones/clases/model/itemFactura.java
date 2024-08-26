18dbcf0968a4945c6f5c76c5ef724e27bdd6bbc1
sofihoyos <sofi.hoyos.2509@gmail.com>
2024-08-26 07:09:50 -0500

    Creacion de los getters y setters del itemFactura

--- src/edu/misena/relaciones/clases/model/itemFactura.java
+++ src/edu/misena/relaciones/clases/model/itemFactura.java
@@ -1,6 +1,14 @@
 package edu.misena.relaciones.clases.model;
 
 public class itemFactura{
-private Producto producto;
-private int cantidad;
+    private Producto producto;
+    private int cantidad;
+
+    //getters//
+    public Producto producto(){return producto;}
+    public int cantidad(){return cantidad;}
+
+    //setters//
+    public void setProducto(Producto producto) {this.producto = producto;}
+    public void setCantidad(int cantidad) {this.cantidad = cantidad;}
 }
